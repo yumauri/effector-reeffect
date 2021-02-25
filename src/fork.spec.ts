63e868fee0c6f3b2de7bddcabbf9e007442fda3d
AlexandrHoroshih <xad71rus@gmail.com>
2021-02-25 21:24:49 +0300

    test(fork): naive test for TAKE_EVERY
    fails though, looks like reeffect breaks allSettled

--- src/fork.spec.ts
+++ src/fork.spec.ts
@@ -1,6 +1,7 @@
 import { createDomain, forward } from 'effector'
 import { fork, serialize, allSettled } from 'effector/fork'
 import { createReEffectFactory } from './createReEffect'
+import { TAKE_EVERY } from './strategy'
 
 test('createReEffect resolves in fork by default', async () => {
   const createReEffect = createReEffectFactory()
@@ -107,4 +108,58 @@ test('createReEffect in fork do not affect domain', async () => {
   })
 
   expect($store.getState()).toMatchInlineSnapshot(`0`)
+})
+
+test('createReEffect in fork: TAKE_EVERY works', async () => {
+  expect.assertions(1)
+  const strategy = TAKE_EVERY
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const triggerOne = app.createEvent()
+  const triggerTwo = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect({
+    handler() {
+
+      return Promise.resolve(2);
+    },
+    strategy
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  forward({
+    from: triggerOne,
+    to: reeffect
+  })
+
+  forward({
+    from: triggerTwo,
+    to: reeffect
+  })
+
+  forward({
+    from: triggerOne,
+    to: triggerTwo,
+  })
+
+  forward({
+    from: start,
+    to: triggerOne
+  });
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 2,
+    }
+  `)
 })
