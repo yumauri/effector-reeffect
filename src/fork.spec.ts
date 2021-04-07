44ee612f7ad7bbbf149bd93a9cb8bc0436349e56
AlexandrHoroshih <xad71rus@gmail.com>
2021-04-07 15:35:22 +0300

    tests(fork): added basic tests for all strategies in fork case

--- src/fork.spec.ts
+++ src/fork.spec.ts
@@ -1,7 +1,7 @@
-import { createDomain, forward } from 'effector'
+import { createDomain, forward, scopeBind } from 'effector'
 import { fork, serialize, allSettled } from 'effector/fork'
 import { createReEffectFactory } from './createReEffect'
-import { TAKE_EVERY } from './strategy'
+import { TAKE_FIRST, TAKE_LAST, QUEUE, RACE } from './strategy'
 
 test('createReEffect resolves in fork by default', async () => {
   const createReEffect = createReEffectFactory()
@@ -110,45 +110,24 @@ test('createReEffect in fork do not affect domain', async () => {
   expect($store.getState()).toMatchInlineSnapshot(`0`)
 })
 
-test('createReEffect in fork: TAKE_EVERY works', async () => {
-  expect.assertions(1)
-  const strategy = TAKE_EVERY
-
+test('createReEffect resolves in scope with scopeBind', async () => {
   const app = createDomain()
   const createReEffect = createReEffectFactory(app.createEffect)
   const start = app.createEvent()
-  const triggerOne = app.createEvent()
-  const triggerTwo = app.createEvent()
   const $store = app.createStore(0, { name: '$store', sid: '$store' })
-  const reeffect = createReEffect({
-    handler() {
-
-      return Promise.resolve(2);
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      return p
     },
-    strategy
+  })
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(5)
   })
 
   $store.on(reeffect.done, (state, { result }) => state + result)
-
-  forward({
-    from: triggerOne,
-    to: reeffect
-  })
-
-  forward({
-    from: triggerTwo,
-    to: reeffect
-  })
-
-  forward({
-    from: triggerOne,
-    to: triggerTwo,
-  })
-
-  forward({
-    from: start,
-    to: triggerOne
-  });
 
   const scope = fork(app)
 
@@ -159,7 +138,349 @@ test('createReEffect in fork: TAKE_EVERY works', async () => {
 
   expect(serialize(scope)).toMatchInlineSnapshot(`
     Object {
-      "$store": 2,
+      "$store": 5,
+    }
+  `)
+})
+
+test('async createReEffect resolves in scope with scopeBind', async () => {
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      await new Promise(r => setTimeout(r, 300))
+
+      return Promise.resolve(p);
+    },
+  })
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(5)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect resolves in scope when called as `inner effect`', async () => {
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      return p
+    },
+  })
+  const effect = app.createEffect(async () => {
+    await reeffect(5)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  forward({
+    from: start,
+    to: effect,
+  })
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect in scope: multiple calls aren`t hanging up `allSettled`', async () => {
+  const cancelled = jest.fn()
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const secondTrigger = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect({
+    async handler() {
+      return new Promise<number>(resolve =>
+        setTimeout(() => resolve(5), 300)
+      )
+    },
+  })
+
+  reeffect.cancelled.watch(cancelled)
+
+  forward({
+    from: start,
+    to: reeffect
+  })
+
+  forward({
+    from: start,
+    to: secondTrigger
+  })
+
+  forward({
+    from: secondTrigger,
+    to: reeffect
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(cancelled).toBeCalledTimes(2)
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect in scope: TAKE_EVERY', async () => {
+  const cancelled = jest.fn()
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      return new Promise<number>(resolve =>
+        setTimeout(() => resolve(p), 300)
+      )
+    },
+  })
+
+  reeffect.cancelled.watch(cancelled)
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(1)
+    bindReeffect(2)
+    bindReeffect(5)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(cancelled).toBeCalledTimes(2)
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect in scope: TAKE_FIRST', async () => {
+  const cancelled = jest.fn()
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      return new Promise<number>(resolve =>
+        setTimeout(() => resolve(p), 300)
+      )
+    },
+    strategy: TAKE_FIRST,
+  })
+
+  reeffect.cancelled.watch(cancelled)
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(5)
+    bindReeffect(2)
+    bindReeffect(3)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(cancelled).toBeCalledTimes(2)
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect in scope: TAKE_LAST', async () => {
+  const cancelled = jest.fn()
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      return new Promise<number>(resolve =>
+        setTimeout(() => resolve(p), 300)
+      )
+    },
+    strategy: TAKE_LAST,
+  })
+
+  reeffect.cancelled.watch(cancelled)
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(5)
+    bindReeffect(2)
+    bindReeffect(5)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(cancelled).toBeCalledTimes(2)
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect in scope: QUEUE', async () => {
+  const cancelled = jest.fn()
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      return new Promise<number>(resolve =>
+        setTimeout(() => resolve(p), 300)
+      )
+    },
+    strategy: QUEUE,
+  })
+
+  reeffect.cancelled.watch(cancelled)
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(1)
+    bindReeffect(2)
+    bindReeffect(3)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(cancelled).toBeCalledTimes(2)
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
+    }
+  `)
+})
+
+test('createReEffect in scope: RACE', async () => {
+  const cancelled = jest.fn()
+
+  const app = createDomain()
+  const createReEffect = createReEffectFactory(app.createEffect)
+  const start = app.createEvent()
+  const $store = app.createStore(0, { name: '$store', sid: '$store' })
+  const reeffect = createReEffect<number, number>({
+    async handler(p) {
+      const timeout = p === 5 ? 100 : 600
+
+      return new Promise<number>(resolve =>
+        setTimeout(() => resolve(p), timeout)
+      )
+    },
+    strategy: RACE,
+  })
+
+  reeffect.cancelled.watch(cancelled)
+
+  start.watch(() => {
+    const bindReeffect = scopeBind(reeffect)
+
+    bindReeffect(2)
+    bindReeffect(2)
+    bindReeffect(5)
+  })
+
+  $store.on(reeffect.done, (state, { result }) => state + result)
+
+  const scope = fork(app)
+
+  await allSettled(start, {
+    scope,
+    params: undefined,
+  })
+
+  expect(cancelled).toBeCalledTimes(2)
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "$store": 5,
     }
   `)
 })
